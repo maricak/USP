88667bcfb427a7ccae10536d899f2e51104e3c55
Marija Kostic <marijakostic25@gmail.com>
2018-06-16 23:06:44 +0200

    finished admin

--- prototip/assets/js/lib/gmap/gmap.init.js
+++ prototip/assets/js/lib/gmap/gmap.init.js
@@ -1,4 +1,24 @@
 (function ($) {
+
+    // var map;
+    // $(document).ready(function () {
+    //     map = new GMaps({
+    //         el: '#basic-map',
+    //         lat: 41.970133,
+    //         lng: -87.6500523,
+    //         zoomControl: true,
+    //         zoomControlOpt: {
+    //             style: 'LARGE',
+    //             position: 'TOP_LEFT'
+    //         },
+    //         panControl: false,
+    //         streetViewControl: false,
+    //         mapTypeControl: false,
+    //         overviewMapControl: false
+    //     });
+    // });
+
+
     var map, infoWindow;
     $(document).ready(function () {
         infoWindow = new google.maps.InfoWindow({});
@@ -6,7 +26,11 @@
             el: '#map-2',
             zoom: 11,
             lat: 41.850033,
-            lng: -87.6500523
+            lng: -87.6500523,
+            click: function (e) {
+                // alert("new trash at location " + e.latLng + "is added");
+                placeMarker(e.latLng, this);
+            }
         });
         map.loadFromFusionTables({
             query: {
@@ -22,5 +46,24 @@
                 }
             }
         });
+        // This event listener calls addMarker() when the map is clicked.
+        map.addListener('click', function (e) {
+            placeMarker(e.latLng, map);
+        });
+
+        function placeMarker(position, map) {
+            var infowindow = new google.maps.InfoWindow({
+                content: 'New trash can is added!'
+            });
+            $('#bootstrap-data-table > tbody:last-child').append('<tr>...</tr><tr>...</tr><tr>...</tr><tr>...</tr><tr>...</tr>');
+
+
+            var marker = new google.maps.Marker({
+                position: position,
+                map: map,
+            });
+            infowindow.open(map, marker);
+            map.panTo(position);
+        }
     });
 })(jQuery);
